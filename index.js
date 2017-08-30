3415a5f5754f432c757afc0c7b67760ff2106048
Eric Nograles <grales@gmail.com>
2017-08-30 07:10:59 -0400

    async sample

--- index.js
+++ index.js
@@ -1,1 +1,18 @@
+const fs = require('fs');
+const path = require('path');
 console.log('hello world');
+
+// Asynchronous operation sample
+fs.readdir(__dirname, (err, files) => {
+  setTimeout(() => {
+    files.map(file => {
+      console.log(file);
+    });
+  }, 300);
+});
+
+fs.readdir(path.join(__dirname, '../'), (err, files) => {
+  files.map(file => {
+    console.log(file);
+  });
+});
