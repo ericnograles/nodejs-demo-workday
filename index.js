f83766ed4bef866006261d45086a6a39cc684e84
Eric Nograles <grales@gmail.com>
2017-08-30 06:59:40 -0400

    standard express scaffold

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-console.log('hello world');
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get('/hello_world', (req, res) => {
+  res.json({message: `hello, ${req.query.name}`});
+});
+
+app.listen(PORT, err => {
+  if (err) {
+    console.error(err);
+  }
+  console.log(`> Ready on port ${PORT}`);
+});
